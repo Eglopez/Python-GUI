0c5013ed7cfeccc65fcb5309d63594619c6642c2
Eduardo <eduardolopezlainez2001@gmail.com>
2019-08-18 21:38:12 -0600

    adding icons

--- MainWindow.py
+++ MainWindow.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 from OtherWindow import *
+from Box import *
 
 class Ui_MainWindow(object):
 
-    def openNewWindow(self):
-        self.window = QtWidgets.QMainWindow()
-        self.ui = Ui_OtherWindow()
-        self.ui.setupUi(self.window)
-        self.window.show()
+    def newBox(self):
+        Dialog = QtWidgets.QDialog()
+        ui = Ui_Dialog()
+        ui.setupUi(Dialog)
+        Dialog.show()
+        Dialog.exec_()
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -28,39 +28,49 @@ class Ui_MainWindow(object):
         self.addtextfile1.setText("")
         self.addtextfile1.setObjectName("addtextfile1")
 
-        self.addtextfile1.clicked.connect(self.openNewWindow)
 
+        self.addtextfile1.setIcon(QtGui.QIcon("text.png"))
+
+        self.addtextfile1.clicked.connect(self.newBox)
 
         self.addfile1 = QtWidgets.QPushButton(self.centralwidget)
         self.addfile1.setGeometry(QtCore.QRect(90, 310, 71, 41))
         self.addfile1.setText("")
         self.addfile1.setObjectName("addfile1")
 
-        self.addfile1.clicked.connect(self.openNewWindow)
-
+        self.addfile1.setIcon(QtGui.QIcon("file.jpg"))
+        self.addfile1.clicked.connect(self.newBox)
 
         self.delete1 = QtWidgets.QPushButton(self.centralwidget)
         self.delete1.setGeometry(QtCore.QRect(180, 310, 61, 41))
         self.delete1.setText("")
         self.delete1.setObjectName("delete1")
+
+        self.delete1.setIcon(QtGui.QIcon("delete.jpg"))
+
         self.addtextfile2 = QtWidgets.QPushButton(self.centralwidget)
         self.addtextfile2.setGeometry(QtCore.QRect(350, 310, 61, 41))
         self.addtextfile2.setText("")
         self.addtextfile2.setObjectName("addtextfile2")
 
-        self.addtextfile2.clicked.connect(self.openNewWindow)
-
+        self.addtextfile2.clicked.connect(self.newBox)
+        self.addtextfile2.setIcon(QtGui.QIcon("text.png"))
 
         self.addfile2 = QtWidgets.QPushButton(self.centralwidget)
         self.addfile2.setGeometry(QtCore.QRect(430, 310, 71, 41))
         self.addfile2.setText("")
         self.addfile2.setObjectName("addfile2")
 
-        self.addfile2.clicked.connect(self.openNewWindow)
+        self.addfile2.clicked.connect(self.newBox)
+        self.addfile2.setIcon(QtGui.QIcon("file.jpg"))
+
         self.delete2 = QtWidgets.QPushButton(self.centralwidget)
         self.delete2.setGeometry(QtCore.QRect(520, 310, 71, 41))
         self.delete2.setText("")
         self.delete2.setObjectName("delete2")
+
+        self.delete2.setIcon(QtGui.QIcon("delete.jpg"))
+
         self.send1 = QtWidgets.QPushButton(self.centralwidget)
         self.send1.setGeometry(QtCore.QRect(250, 100, 89, 25))
         self.send1.setText("")
@@ -80,6 +90,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowIcon(QtGui.QIcon("images.png"))
 
 
 if __name__ == "__main__":
